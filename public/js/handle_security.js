5a2c394aba4f3faf553e867669bdca2e62d11916
Nhựt Trường Võ <cristruong321@gmail.com>
2022-02-18 19:59:57 +0700

    Add footer

--- public/js/handle_security.js
+++ public/js/handle_security.js
@@ -1,12 +1,19 @@
 $(document).ready(() => {
     // init modal security
+
+    $("#btn_call_security").click(() => {
+        $("#btn_toggle_security").click();
+    });
+
     $("#btn_toggle_security").click(() => {
-        let passCurrent = $("#current_pass").val();
+        let passCurrent = $("#current_pass").val().trim();
         $("#password").val(passCurrent);
         if (passCurrent !== "") {
+            console.log("True");
             $("#switch_toggle_pass").prop("checked", true);
             $("#password").show();
         } else {
+            console.log("False");
             $("#switch_toggle_pass").prop("checked", false);
             $("#password").hide();
         }
